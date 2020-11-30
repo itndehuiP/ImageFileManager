94c86c9ef2f16605c889569db493814e1aa56564
itndehuiP <itandehui_pacheco@hotmail.com>
2020-11-30 11:44:14 -0500

    First commit

--- Sources/ImageFileManager/ImageFileManager.swift
+++ Sources/ImageFileManager/ImageFileManager.swift
@@ -1,3 +1,91 @@
-struct ImageFileManager {
-    var text = "Hello, World!"
+import Foundation
+
+public struct ImageFileManager {
+    
+    public init() {}
+    private let mainFolder = "ImageManager"
+    
+    private var directoryPath: URL {
+      let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+      let folderDirectoryURL = paths.first!.appendingPathComponent(mainFolder)
+        return folderDirectoryURL
+    }
+    
+    private func directoryPath(createIfNeeded: Bool) -> URL {
+        if createIfNeeded {
+            do {
+              try FileManager.default.createDirectory(at: directoryPath,
+                                                      withIntermediateDirectories: true,
+                                                      attributes: nil)
+            } catch {
+                  print("Couldn't create directory \(error.localizedDescription)")
+            }
+        }
+        return directoryPath
+    }
+    
+    private func albumDirectoryPath(album: String, createIfNeeded: Bool) -> URL? {
+        let albumDirectoryURL = directoryPath(createIfNeeded: createIfNeeded).appendingPathComponent(album, isDirectory: true)
+        if createIfNeeded {
+            do {
+                try FileManager.default.createDirectory(at: albumDirectoryURL,
+                                                        withIntermediateDirectories: true,
+                                                        attributes: nil)
+            } catch {
+                print("Couldn't create directory \(error.localizedDescription)")
+            }
+        }
+        return albumDirectoryURL
+    }
+    
+    private func dataDirectoryPath(album: String? = nil, with id: String, createIfNeeded: Bool) -> URL? {
+        guard !id.isEmpty else { return nil }
+        if let album = album {
+            return albumDirectoryPath(album: album, createIfNeeded: createIfNeeded)?.appendingPathComponent(id)
+        } else {
+            return directoryPath(createIfNeeded: createIfNeeded).appendingPathComponent(id)
+        }
+    }
+    
+    public func saveImage(data: Data?, album: String? = nil, id: String?) -> URL? {
+        guard let data = data, let id = id, let dataPath = dataDirectoryPath(album: album, with: id, createIfNeeded: true) else { return nil }
+        do {
+            try data.write(to: dataPath)
+        } catch {
+            print("Couldn't write to save file: " + error.localizedDescription)
+        }
+        return dataPath
+    }
+    
+    public func loadImage(album: String? = nil, id: String?) -> Data? {
+        guard let id = id, let dataPath = dataDirectoryPath(album: album, with: id, createIfNeeded: false) else { return nil }
+        return try? Data(contentsOf: dataPath)
+    }
+    
+    //MARK: Delete
+    public func deleteAll() {
+        do {
+            try FileManager.default.removeItem(at: directoryPath(createIfNeeded: false))
+        } catch {
+            print("Error Deleting Folder. " + error.localizedDescription)
+        }
+    }
+    
+    public func delete(album: String) {
+        guard let albumPath = albumDirectoryPath(album: album, createIfNeeded: false) else { return }
+        do {
+            try FileManager.default.removeItem(at: albumPath)
+        } catch {
+            print("Error Deleting Folder. " + error.localizedDescription)
+        }
+    }
+    
+    public func deleteImage(album: String? = nil, id: String?) {
+        guard let id = id, let dataPath = dataDirectoryPath(album: album, with: id, createIfNeeded: false) else { return }
+        do {
+            try FileManager.default.removeItem(at: dataPath)
+        } catch {
+            print("Error Deleting Folder. " + error.localizedDescription)
+        }
+    }
 }
--- Tests/ImageFileManagerTests/ImageFileManagerTests.swift
+++ Tests/ImageFileManagerTests/ImageFileManagerTests.swift
@@ -1,15 +1,100 @@
 import XCTest
 @testable import ImageFileManager
 
+
+struct DataMock: Codable {
+    var name: String
+    var value: Int
+}
+
 final class ImageFileManagerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ImageFileManager().text, "Hello, World!")
+    
+    let imageManager = ImageFileManager()
+    
+    func testSaveWithoutFolder() {
+        let mock = DataMock(name: "TestA", value: 0)
+        let data = try? JSONEncoder().encode(mock)
+        XCTAssertNotNil(data)
+        let url = imageManager.saveImage(data: data, id: "TestA")
+        XCTAssertNotNil(url)
     }
+    
+    func testLoadWithoutFolder() {
+        let mock = DataMock(name: "TestB", value: 0)
+        let data = try? JSONEncoder().encode(mock)
+        let mockQ = try! JSONDecoder().decode(DataMock.self, from: data!)
+        XCTAssertEqual(mockQ.name, "TestB")
+        XCTAssertNotNil(data)
+        let url = imageManager.saveImage(data: data, id: "TestB")
+        XCTAssertNotNil(url)
+        let dataF = imageManager.loadImage(id: "TestB")
+        XCTAssertNotNil(dataF)
+        let mockR = try! JSONDecoder().decode(DataMock.self, from: dataF!)
+        XCTAssertEqual(mockR.name, "TestB")
+    }
+    
+    func testDeleteWithoutFolder() {
+        let mock = DataMock(name: "TestC", value: 0)
+        let data = try? JSONEncoder().encode(mock)
+        XCTAssertNotNil(data)
+        let url = imageManager.saveImage(data: data, id: "TestC")
+        XCTAssertNotNil(url)
+        let dataF = imageManager.loadImage(id: "TestC")
+        XCTAssertNotNil(dataF)
+        imageManager.deleteImage(id: "TestC")
+        let dataA = imageManager.loadImage(id: "TestC")
+        XCTAssertNil(dataA)
+    }
+    
+    func testSaveWithFolder() {
+        let mock = DataMock(name: "TestFolder", value: 0)
+        let data = try? JSONEncoder().encode(mock)
+        XCTAssertNotNil(data)
+        let url = imageManager.saveImage(data: data, album: "AlbumFolder", id: "Test")
+        XCTAssertNotNil(url)
+    }
+    
+    func testLoadWithFolder() {
+        let mock = DataMock(name: "Test", value: 0)
+        let data = try? JSONEncoder().encode(mock)
+        XCTAssertNotNil(data)
+        let url = imageManager.saveImage(data: data, album: "AlbumFolderA", id: "Test")
+        XCTAssertNotNil(url)
+        let dataLoaded = imageManager.loadImage(album: "AlbumFolderA", id: "Test")
+        XCTAssertNotNil(dataLoaded)
+    }
+    
+    func testDeleteWithFolder() {
+        let mock = DataMock(name: "Test", value: 0)
+        let data = try? JSONEncoder().encode(mock)
+        XCTAssertNotNil(data)
+        let url = imageManager.saveImage(data: data, album: "AlbumFolderB", id: "Test")
+        XCTAssertNotNil(url)
+        var dataLoaded = imageManager.loadImage(album: "AlbumFolderB", id: "Test")
+        XCTAssertNotNil(dataLoaded)
+        imageManager.deleteImage(album: "AlbumFolderB", id: "Test")
+        dataLoaded = imageManager.loadImage(album: "AlbumFolderB", id: "Test")
+        XCTAssertNil(dataLoaded)
+    }
+    
+    func testDeleteAlbumFolder() {
+        let mock = DataMock(name: "Test", value: 0)
+        let data = try? JSONEncoder().encode(mock)
+        XCTAssertNotNil(data)
+        let url = imageManager.saveImage(data: data, album: "AlbumFolderC", id: "Test")
+        XCTAssertNotNil(url)
+        var dataLoaded = imageManager.loadImage(album: "AlbumFolderC", id: "Test")
+        XCTAssertNotNil(dataLoaded)
+        imageManager.delete(album: "AlbumFolderC")
+        dataLoaded = imageManager.loadImage(album: "AlbumFolderC", id: "Test")
+        XCTAssertNil(dataLoaded)
+    }
+//    //MARK: Important
+//    func testRemoveTestFolder() {
+//        imageManager.deleteAll()
+//    }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testSaveWithoutFolder", testSaveWithoutFolder),
     ]
 }
